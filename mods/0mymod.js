b0916772da4ec155521ab508bc7c5a195698cc8e
DaKittengamer <kittengamer002@gmail.com>
2026-03-31 15:44:55 -0500

    Update 0mymod.js

--- mods/0mymod.js
+++ mods/0mymod.js
@@ -1,10 +1,9 @@
 elements.decamethyldizincocene = {
-    color: ["#800080", "#FF00FF"],
+    color: ["#80008000"],
     behavior: behaviors.WALL,
     category: "solids",
     state: "solid",
     density: 2500,
-    desc: "Potassium permanganate is a dark purple, crystalline solid. It is used as a powerful oxidizing agent.",
     tempHigh: 110,
-    stateHigh:
+    stateHigh: "air",
 }
