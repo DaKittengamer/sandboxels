375f7d2463655a488c8d5197ff0138a66b7dbb5b
DaKittengamer <kittengamer002@gmail.com>
2026-03-31 15:39:06 -0500

    Change behavior and category of decamethyldizincocene

--- mods/0mymod.js
+++ mods/0mymod.js
@@ -1,7 +1,7 @@
 elements.decamethyldizincocene = {
     color: ["#800080", "#FF00FF"],
-    behavior: behaviors.POWDER,
-    category: "powders",
+    behavior: behaviors.WALL,
+    category: "solids",
     state: "solid",
     density: 2500,
     desc: "Potassium permanganate is a dark purple, crystalline solid. It is used as a powerful oxidizing agent.",
