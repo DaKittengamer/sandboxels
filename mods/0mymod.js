2f6ed07fa4f5f3b844618a9ff6a933ee13730b73
DaKittengamer <kittengamer002@gmail.com>
2026-03-31 15:38:14 -0500

    Define decamethyldizincocene element attributes
    Add properties for decamethyldizincocene element.

--- mods/0mymod.js
+++ mods/0mymod.js
@@ -1,1 +1,10 @@
-
+elements.decamethyldizincocene = {
+    color: ["#800080", "#FF00FF"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state: "solid",
+    density: 2500,
+    desc: "Potassium permanganate is a dark purple, crystalline solid. It is used as a powerful oxidizing agent.",
+    tempHigh: 110,
+    stateHigh:
+}
